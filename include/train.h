5c9e55f727128bf22a0324088af731666c98d432
pavlushatkachev-max <pavlushatkachev@gmail.com>
2026-05-30 16:27:09 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -1,6 +1,7 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TRAIN_H_
 #define INCLUDE_TRAIN_H_
+
 class Train {
  private:
   struct Car {
@@ -9,12 +10,17 @@ class Train {
     Car *prev;
   };
   int countOp;
-  Car *cur;
+  Car *first;
+  Car* createCar(bool light) {
+    Car* temp = new Car;
+    temp->light = light;
+    temp->prev = temp->next = nullptr;
+    return temp;
+  }
  public:
-  Train();
-  ~Train();
+  Train(): first(nullptr), countOp(0) {}
   void addCar(bool light);
   int getLength();
-  int getOpCount() const;
+  int getOpCount();
 };
 #endif  // INCLUDE_TRAIN_H_
