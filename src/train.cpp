11bbf2f66d7388df460abc4b45c9f7632ff65a37
pavlushatkachev-max <pavlushatkachev@gmail.com>
2026-05-30 16:28:14 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,73 +1,33 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
-
-Train::Train() : countOp(0), first(nullptr) {}
-
-Train::~Train() {
-  if (!first) return;
-  Car *cur = first;
-  do {
-    Car *nxt = cur->next;
-    delete cur;
-    cur = nxt;
-  } while (cur != first);
-}
-
+#include <iostream>
 void Train::addCar(bool light) {
-  Car *newCar = new Car{light, nullptr, nullptr};
-  if (!first) {
-    first = newCar;
+  if (first == nullptr) {
+    first = createCar(light);
     first->next = first;
     first->prev = first;
   } else {
-    Car *last = first->prev;
-    newCar->next = first;
-    newCar->prev = last;
-    last->next = newCar;
-    first->prev = newCar;
+    Car* item = createCar(light);
+    item->prev = first->prev;
+    item->next = first;
+    first->prev->next = item;
+    first->prev = item;
   }
 }
-
 int Train::getLength() {
-  if (!first) return 0;
-
-  if (!first->light) {
     first->light = true;
-    Car *cur = first;
-    int steps = 0;
-    do {
-      cur = cur->next;
-      ++countOp;
-      ++steps;
-    } while (cur != first);
-    for (int i = 0; i < steps; ++i) {
-      cur = cur->prev;
-      ++countOp;
+    const Car* temp = first->next;
+    int count = 1;
+    while (temp != first) {
+        if (temp->light == true) {
+            countOp += (count * 2);
+        }
+        temp = temp->next;
+        count++;
     }
-    return steps;
-  } else {
-    first->light = true;
-    int k = 1;
-    while (true) {
-      Car *cur = first;
-      for (int i = 0; i < k; ++i) {
-        cur = cur->next;
-        ++countOp;
-        cur->light = false;
-      }
-      for (int i = 0; i < k; ++i) {
-        cur = cur->prev;
-        ++countOp;
-      }
-      if (!first->light) {
-        break;
-      }
-      ++k;
-    }
-    return k;
-  }
+    countOp += (count * 2);
+    return count;
 }
-
-int Train::getOpCount() const {
+int Train::getOpCount() {
   return countOp;
 }
