e3c33ca8a381bac8c5ef362224179796484c0950
pavlushatkachev-max <pavlushatkachev@gmail.com>
2026-05-30 16:27:54 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,44 @@
 // Copyright 2022 NNTU-CS
 #include <iostream>
-#include <fstream>
-#include <cstdlib>
-#include <ctime>
-#include <string>
 #include "train.h"
-using std::cout;
-using std::ofstream;
-using std::srand;
-using std::time;
 int main() {
-  const int kMinN = 10;
-  const int kMaxN = 200;
-  const int kStep = 10;
-  const int kTrialsRandom = 10;
-  ofstream out("experiment.csv");
-  out << "n,all_off,all_on,random_avg\n";
-  unsigned seed = static_cast<unsigned>(time(nullptr));
-  auto myRand = [&seed]() -> int {
-    seed = seed * 1103515245 + 12345;
-    return static_cast<int>((seed / 65536) % 32768);
-  };
-  for (int n = kMinN; n <= kMaxN; n += kStep) {
-    Train t_off;
-    for (int i = 0; i < n; ++i) {
-      t_off.addCar(false);
+  int size[] = { 500, 800, 1000, 1500, 2000, 4000, 5000, 6000, 8000, 10000,
+    25000, 30000, 40000, 50000, 60000, 120000, 150000, 200000, 250000, 300000,
+    400000, 500000, 600000, 700000, 800000, 900000, 1000000, 0};
+  for (int i = 0; size[i]; i++) {
+    clock_t begin1, end1, begin2, end2, begin3, end3;
+    clock_t t1, t2, t3;
+    int j = size[i];
+    Train train1;
+    while (j--)
+      train1.addCar(false);
+    begin1 = clock();
+    train1.getLength();
+    train1.getOpCount();
+    end1 = clock();
+    j = size[i];
+    Train train2;
+    while (j--)
+      train2.addCar(true);
+    begin2 = clock();
+    train2.getLength();
+    train2.getOpCount();
+    end2 = clock();
+    j = size[i];
+    Train train3;
+    while (j--) {
+      if (j % 2 == 0)
+        train3.addCar(true);
+      else
+        train3.addCar(false);
     }
-    t_off.getLength();
-    int ops_off = t_off.getOpCount();
-    Train t_on;
-    for (int i = 0; i < n; ++i) {
-      t_on.addCar(true);
-    }
-    t_on.getLength();
-    int ops_on = t_on.getOpCount();
-    double sum_random = 0.0;
-    for (int t = 0; t < kTrialsRandom; ++t) {
-      Train t_rand;
-      for (int i = 0; i < n; ++i) {
-        bool light = (myRand() % 2 == 1);
-        t_rand.addCar(light);
-      }
-      t_rand.getLength();
-      sum_random += t_rand.getOpCount();
-    }
-    double avg_random = sum_random / kTrialsRandom;
-    out << n << "," << ops_off << "," << ops_on << "," << avg_random << "\n";
+    begin3 = clock();
+    train3.getLength();
+    train3.getOpCount();
+    end3 = clock();
+    t1 = end1 - begin1;
+    t2 = end2 - begin2;
+    t3 = end3 - begin3;
+    std::cout << t1 << " " << t2 << " " << t3 << std::endl;
   }
-  out.close();
-  cout << "Data saved to experiment.csv\n";
-  return 0;
 }
